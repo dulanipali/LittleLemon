74d9807f0fb44e629b0244024be05b8780db197a
dulanipali <dulani.2020@gmail.com>
2025-07-01 16:59:56 -0500

    Added Semantic structure, OGP

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <img src="logo.jpg" alt="Little Lemon Logo"></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main>
+            <h1>Welcome to Little LEmon</h1>
+        </main>
+    )
+}
+
+export default Main;
